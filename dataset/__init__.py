6f7bfc31cc6be114d1bf363a6e3b34002bee8061
Steve <peviroy@outlook.com>
2020-09-10 19:12:59 +0800

    refactor(merge): fix merge problem

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,5 +1,8 @@
-from ._dataset import get_sms_dataset
+from ._sms import get_sms_dataset
+from ._sms_dataset import SMSDataset, SMSTransform
 
 __all__ = [
-    'get_sms_dataset'
+    'get_sms_dataset',
+    'SMSDataset',
+    'SMSTransform'
 ]
